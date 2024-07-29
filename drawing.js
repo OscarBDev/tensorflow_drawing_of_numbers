da7381b45384816964b4e8714a3b8bbdcaf099d7
Hata Siu <for.all.today123@gmail.com>
2024-07-28 20:10:30 -0400

    second commit

--- drawing.js
+++ drawing.js
@@ -1,5 +1,7 @@
 var brushWidth = 10;
 var color = "#000000";
+
+//primer predict ------------------------------------------------------------------------------------
 var drawingcanvas;
 
 (function() {
@@ -14,5 +16,24 @@ var drawingcanvas;
   if (drawingcanvas.freeDrawingBrush) {
     drawingcanvas.freeDrawingBrush.color = color;
     drawingcanvas.freeDrawingBrush.width = brushWidth;
+  }
+})();
+
+//segundo predict -----------------------------------------------------------------------------------
+
+var drawingcanvas2;
+
+(function() {
+  var $ = function(id){return document.getElementById(id)};
+
+  drawingcanvas2 = this.__canvas = new fabric.Canvas('bigcanvas2', {
+    isDrawingMode: true
+  });
+
+  fabric.Object.prototype.transparentCorners = false;
+
+  if (drawingcanvas2.freeDrawingBrush) {
+    drawingcanvas2.freeDrawingBrush.color = color;
+    drawingcanvas2.freeDrawingBrush.width = brushWidth;
   }
 })();
